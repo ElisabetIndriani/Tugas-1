60b9f1cf59d17f7876be812dbd5d56b356c05395
ElisabetIndriani <elisabetindri06@gmail.com>
2021-02-14 16:49:04 +0700

    Tugas- 1

--- Soal-2.py
+++ Soal-2.py
@@ -3,6 +3,6 @@ r=input("jari-jari lingkaran :")
 b= float(r)
 x= (22/7)*b*b
 e= "{:.2f}".format(x)
-c= "luas lingkaran dengan jari-jari" + " "+ r
+c= "luas lingkaran dengan jari-jari" + " "+ r +" "+ "cm"
 d= "adalah" +" "+ str(e)
 print(c+" "+d+" cm\u00b2")
